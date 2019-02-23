d10c53ff5651e1fd8ce735cd1ed4c09ce440d244
shawn5141 <s19911111@gmail.com>
2019-02-22 23:43:33 -0800

    g.py

--- g.py
+++ g.py
@@ -15,12 +15,17 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 import matplotlib.pyplot as plt
 import pandas as pd
+import shutil
+from sys import exit
+
 
 EPOCH = 10
 BATCH_SIZE = 64
 LR = 0.005
 torch.manual_seed(1) 
 BATCH_SIZE = 10
+
+
 
 class AutoEncoder(nn.Module):
     def __init__(self):
@@ -75,8 +80,9 @@ def PCA_transform(img):
     nComp = 25
     Xhat = np.dot(pca.transform(img)[:,:nComp], pca.components_[:nComp,:])+np.mean(img, axis=0)
     print(np.sum((img-Xhat)**2)/len(img)/400)
+    return np.sum((img-Xhat)**2)/len(img)/400
 
-def AutoEncode(train,test,LR):
+def AutoEncode(train,test,LR,re_error):
 
     def PCA_transform(img):
         pca=PCA(n_components=25)
@@ -115,6 +121,8 @@ def AutoEncode(train,test,LR):
             optimizer.step()                    # apply gradients
             #print('Epoch: ', epoch, '| train loss: %.4f' % (loss/400))
     Loss=[]
+    Special_test=[]
+    Special_num=[]
    
     for step, (x, b_label) in enumerate(test):
         
@@ -125,13 +133,19 @@ def AutoEncode(train,test,LR):
             b_y=Variable(b_y).cuda()
         encoded, decoded = autoencoder(b_x)
         #loss=loss_func(decoded,b_y)
-        loss = loss_func(torch.mul(decoded,255), torch.mul(b_y,255))      # mean square error
+        loss = loss_func(torch.mul(decoded,255), torch.mul(b_y,255))      # mean square error   
+        
+        if loss < re_error:
+            #Special_test.append([step,b_x.cpu().detach().numpy()])
+            Special_test.append(step)
+            
         #print(loss)
         Loss.append(loss.cpu().detach().numpy())
     Loss=np.array(Loss).reshape(-1,len(Loss))
     #print(Loss.shape[1])
-    Loss=np.sum(np.squeeze(Loss))/Loss.shape[1]/400
+    Loss=np.sum(np.squeeze(Loss))/Loss.shape[1]
     print(Loss)
+    print(Special_test)
         #if epoch % 10 == 0:
             #pic = to_img(output.cpu().data)
             #save_image(pic, './mlp_img/image_{}.png'.format(epoch))
@@ -139,10 +153,33 @@ def AutoEncode(train,test,LR):
     #torch.save(model.state_dict(), './sim_autoencoder.pth')
 
 if __name__ == '__main__':
-    train_imgs,train_loader = LoadDir('galaxy/train')
+    #train_imgs,train_loader = LoadDir('galaxy/train')
+    #test_imgs,test_loader=LoadDir('galaxy/test')
+    train_imgs,train_loader = LoadDir('galaxy/psudo1')
     test_imgs,test_loader=LoadDir('galaxy/test')
-    PCA_transform(train_imgs)
-    PCA_transform(test_imgs)
-    AutoEncode(train_loader,test_loader,LR)
-   
+    re_error_val=PCA_transform(train_imgs)
+    re_error_test=PCA_transform(test_imgs)
+    AutoEncode(train_loader,test_loader,LR,re_error_test)
+    
+
+    source = "/home/shawn/machine/Machine-Learning/galaxy/test/"
+    target = "/home/shawn/machine/Machine-Learning/galaxy/Special_graph/"
+    i=0
+    g=open('number_for_special.txt','r')
+    lines=g.read()
+    lines=lines.strip("[]\n").split(", ")
+    for filename in os.listdir('galaxy/test'):
+        for l in lines:
+            print(l,i)
+            if int(l)==i:
+                print("low")
+                shutil.copy(source+str(filename),target+str(filename))
+        i+=1
+            
+                
+
+        
+
+
+
  
